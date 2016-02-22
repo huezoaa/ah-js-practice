1a2e1ee4cd4c322924bebecd283d0ddbeb6e0638
Angel Huezo <angelohuezo@gmail.com>
2016-02-21 23:16:44 -0500

    Examples of selecting by <li>  list item.  I added a table with 2 columns to the html.  Used all the css from GSD and I played around a little with fadeOut(‘slow’).  Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   stylesheet.css

--- script.js
+++ script.js
@@ -9,7 +9,23 @@ $(document).ready(function() {
            height: '-=10px'
        });
    });
-   $('div').click(function() {
-       $(this).toggle(1000);
-   });
+
+   // $('div').click(function() {
+   //     $(this).toggle(1000);
+   // });
+
+  $('div').click(function(){
+    $(this).fadeOut('slow');
+  });
+
+  // $('li:last-child').click(function(){
+  //   $(this).toggle(500);
+  // });
+
+//Example of selecting by list item tag:
+  $('li').click(function(){
+    $(this).fadeOut('slow');
+  });
+
+
 });
