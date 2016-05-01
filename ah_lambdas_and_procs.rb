d4522f25d4d9e8187a7d0d18bf4e870089027b48
Angel Huezo <angel.huezo@kipusystems.com>
2016-05-01 18:36:58 -0400

    Improving readability

--- ah_lambdas_and_procs.rb
+++ ah_lambdas_and_procs.rb
@@ -1,10 +1,13 @@
 # Example of calling lambdas:
 
-def dashes
+def title(text='')
   30.times{print '*'}
-  puts
+  puts "\n", text
+
+  2.times{puts "\n"}
 end
 
+title('Example of calling lambdas')
 my_lambda = lambda { puts "Lambda called."}
 
 my_lambda.call
@@ -14,13 +17,18 @@ my_lambda.===
 
 
 # Lambda with arguments
+title('Lambda with one argument')
 
 times_two = ->(x) { x * 2 }
 puts times_two.call(10)
 
 
+# Lambda with 2 of 2 arguments
+title('Lambda with 2 of 2 arguments')
 my_lambda = ->(x,y){puts x * y}
 my_lambda.call(2,3)
 
+# Proc with 2 of 2 arguments
+title('Proc with 2 of 2 arguments')
 my_proc = Proc.new {|x,y| puts x * y}
 my_proc.call(3,4)
