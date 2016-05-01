97ffbf68090cf2e5c10e833abcf332d6b972a2e9
Angel Huezo <angel.huezo@kipusystems.com>
2016-05-01 19:34:13 -0400

    Further testing.  Examples of proc and lambda with incorrect number of arguments and how they behave. Changes to be committed: modified:   ah_lambdas_and_procs.rb

--- ah_lambdas_and_procs.rb
+++ ah_lambdas_and_procs.rb
@@ -3,12 +3,12 @@
 def title(text='')
   30.times{print '*'}
   puts "\n", text
-
   2.times{puts "\n"}
 end
 
+# Example of how to call lambdas:
 title('Example of calling lambdas')
-my_lambda = lambda { puts "Lambda called."}
+my_lambda = -> { puts "Lambda called."}
 
 my_lambda.call
 my_lambda.()
@@ -22,6 +22,9 @@ title('Lambda with one argument')
 times_two = ->(x) { x * 2 }
 puts times_two.call(10)
 
+times_three = lambda{ |y| y * 3}
+puts times_three.call(3)
+
 
 # Lambda with 2 of 2 arguments
 title('Lambda with 2 of 2 arguments')
@@ -32,3 +35,14 @@ my_lambda.call(2,3)
 title('Proc with 2 of 2 arguments')
 my_proc = Proc.new {|x,y| puts x * y}
 my_proc.call(3,4)
+
+# Lambda with 1 of 2 arguments. Error expected
+title('Lambda with only 1 of 2 arguments.  Error expected')
+my_lambda = ->(x,y){puts "No errors here!"}
+my_lambda.call(5)
+
+# Proc with 1 of 2 arguments.  No error expected
+title('Proc with 1 of 2 arguments. No error expected')
+my_proc = Proc.new {|x,y| puts "No errors here!"}
+my_proc.call(6)
+
